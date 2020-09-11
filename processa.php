f42686571ef8ac8c63c87028a53e3992cc31496d
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-09-11 02:17:11 -0300

    teste sem id

--- processa.php
+++ processa.php
@@ -2,7 +2,7 @@
     session_start();
     include_once("conexao.php");
 
-    $id = filter_input(INPUT_POST, 'id_livro', FILTER_SANITIZE_NUMBER_INT);
+    #$id = filter_input(INPUT_POST, 'id_livro', FILTER_SANITIZE_NUMBER_INT);
     $titulo = filter_input(INPUT_POST, 'titulo_livro', FILTER_SANITIZE_STRING);
     $isbn = filter_input(INPUT_POST, 'isbn_livro', FILTER_SANITIZE_NUMBER_INT);
     $autor = filter_input(INPUT_POST, 'autor_livro', FILTER_SANITIZE_STRING);
@@ -10,7 +10,7 @@
     $sessao = filter_input(INPUT_POST, 'sessao_livro', FILTER_SANITIZE_STRING);
     
 
-    $result_user = "INSERT INTO livros VALUES ('$id', '$titulo', '$isbn', '$autor', '$edicao', '$sessao')";
+    $result_user = "INSERT INTO livros VALUES ('$titulo', '$isbn', '$autor', '$edicao', '$sessao')";
     $result_query = pg_query($conexao, $result_user);
 
     if($result_query){
