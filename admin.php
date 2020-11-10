f6caed90b8e65627353ff9052c50d6ae5b74f773
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-10 05:40:03 -0300

    teste final tabela

--- admin.php
+++ admin.php
@@ -98,7 +98,8 @@
                             </tr>
                             <tr>
                                 <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
+                                <td>Larry</td>
+                                <td>the Bird</td>
                                 <td>@twitter</td>
                             </tr>
                         </tbody>
