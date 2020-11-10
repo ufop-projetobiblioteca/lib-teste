613cf97959bd807a62cde7ac729b38ff9c8dbf05
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-10 03:56:17 -0300

    testando nivel de acesso

--- valida_login.php
+++ valida_login.php
@@ -7,21 +7,15 @@
         
         if((!empty($usuario)) AND (!empty($senha))){          
             //pesquisar usuário no BD
-            $result_usuario = "SELECT * FROM usuarios WHERE email='$usuario'";
+            $result_usuario = "SELECT * FROM usuarios WHERE email='$usuario' and senha='$senha'";
             $resultado_usuario = pg_query($conexao, $result_usuario);
             
             if($resultado_usuario){
                 $row_usuario = pg_fetch_assoc($resultado_usuario);
-                if($senha == $row_usuario['senha']){
-                    $_SESSION['msg'] = "Deu bom!";
-                    header("Location: index.php");
-                    
-                    $_SESSION['cpf'] = $row_usuario['cpf'];
-                    $_SESSION['pnome'] = $row_usuario['pnome'];
-                    $_SESSION['email'] = $row_usuario['email'];
+                if($row_usuario['tipo_usuario']==1){
+                    header("Location: admin.php");
                 }else{
-                    $_SESSION['msg'] = "Usuário ou Senha incorretos!";
-                    header("Location: index.php");
+                    header("Location: usuario.php");
                 }  
             }
         }
