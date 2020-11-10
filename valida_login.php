3e958c29940da3a8e84632496626bb1e447ec6b7
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-10 04:01:32 -0300

    testando nível de acesso

--- valida_login.php
+++ valida_login.php
@@ -1,24 +1,24 @@
 <?php
-    session_start();
-    include_once("conexao.php");
-    {
-        $usuario = filter_input(INPUT_POST, 'inputEmail', FILTER_SANITIZE_EMAIL);
-        $senha = filter_input(INPUT_POST, 'inputPassword', FILTER_SANITIZE_STRING);
-        
-        if((!empty($usuario)) AND (!empty($senha))){          
-            //pesquisar usuário no BD
-            $result_usuario = "SELECT * FROM usuarios WHERE email='$usuario' and senha='$senha'";
-            $resultado_usuario = pg_query($conexao, $result_usuario);
-            
-            if($resultado_usuario){
-                $row_usuario = pg_fetch_assoc($resultado_usuario);
-                if($row_usuario['tipo_usuario']==1){
-                    header("Location: admin.php");
-                }else{
-                    header("Location: usuario.php");
-                }  
+session_start();
+include_once("conexao.php"); {
+    $usuario = filter_input(INPUT_POST, 'inputEmail', FILTER_SANITIZE_EMAIL);
+    $senha = filter_input(INPUT_POST, 'inputPassword', FILTER_SANITIZE_STRING);
+
+    if ((!empty($usuario)) and (!empty($senha))) {
+        //pesquisar usuário no BD
+        $result_usuario = "SELECT * FROM usuarios WHERE email='$usuario' and senha='$senha'";
+        $resultado_usuario = pg_query($conexao, $result_usuario);
+
+        if ($resultado_usuario) {
+            $row_usuario = pg_fetch_assoc($resultado_usuario);
+            if ($row_usuario['tipo_usuario'] == 1) {
+                header("Location: admin.php");
+            } else if ($row_usuario['tipo_usuario'] == 0) {
+                header("Location: usuario.php");
             }
+        } else {
+            $_SESSION['msg'] = "Usuário ou senha incorretos!";
+            header("Location: index.php");
         }
     }
-?>
-
+}
