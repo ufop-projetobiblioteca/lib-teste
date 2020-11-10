46868acc8438136bd64a3e5f9533d7b55b1d5f65
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-10 04:34:28 -0300

    testando area administrativa

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!doctype html>
 <?php
-session_start();
+  session_start();
+  if (!isset($_SESSION['cpf'])) {
+    $_SESSION['msg'] = 'Faça o Login para continuar!';
+    header("Location: index.php");
+  }
 ?>
 <html lang="pt-br">
 
--- valida_login.php
+++ valida_login.php
@@ -1,29 +1,37 @@
 <?php
 session_start();
-include_once("conexao.php"); {
-    $usuario = filter_input(INPUT_POST, 'inputEmail', FILTER_SANITIZE_EMAIL);
-    $senha = filter_input(INPUT_POST, 'inputPassword', FILTER_SANITIZE_STRING);
+include_once("conexao.php");
 
-    if ((!empty($usuario)) and (!empty($senha))) {
-        //pesquisar usuário no BD
-        $result_usuario = "SELECT * FROM usuarios WHERE email='$usuario'";
-        $resultado_usuario = pg_query($conexao, $result_usuario);
+$usuario = filter_input(INPUT_POST, 'inputEmail', FILTER_SANITIZE_EMAIL);
+$senha = filter_input(INPUT_POST, 'inputPassword', FILTER_SANITIZE_STRING);
 
-        if ($resultado_usuario) {
-            $row_usuario = pg_fetch_assoc($resultado_usuario);
-            if ($senha == $row_usuario['senha']) {
-                if ($row_usuario['tipo_usuario'] == 1) {
-                    header("Location: admin.php");
-                } else {
-                    header("Location: usuario.php");
-                }
+if ((!empty($usuario)) and (!empty($senha))) {
+    //pesquisar usuário no BD
+    $result_usuario = "SELECT * FROM usuarios WHERE email='$usuario'";
+    $resultado_usuario = pg_query($conexao, $result_usuario);
+
+    if ($resultado_usuario) {
+        $row_usuario = pg_fetch_assoc($resultado_usuario);
+        if ($senha == $row_usuario['senha']) {
+            if ($row_usuario['tipo_usuario'] == 1) {
+                $_SESSION['cpf'] = $row_usuario['cpf'];
+                $_SESSION['pnome'] = $row_usuario['pnome'];
+                $_SESSION['email'] = $row_usuario['email'];
+
+                header("Location: admin.php");
             } else {
-                $_SESSION['msg'] = "Usuário ou Senha incorretos!";
-                header("Location: index.php");
+                $_SESSION['cpf'] = $row_usuario['cpf'];
+                $_SESSION['pnome'] = $row_usuario['pnome'];
+                $_SESSION['email'] = $row_usuario['email'];
+
+                header("Location: usuario.php");
             }
+        } else {
+            $_SESSION['msg'] = "Usuário ou Senha incorretos!";
+            header("Location: index.php");
         }
-    } else {
-        $_SESSION['msg'] = "Página não encontrada";
-        header("Location: index.php");
     }
+} else {
+    $_SESSION['msg'] = "Página não encontrada";
+    header("Location: index.php");
 }
