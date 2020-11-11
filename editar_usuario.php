180ea25af42a35ee33a74b495f5b5190918b187a
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-11 14:06:55 -0300

    testando editar usuario

--- editar_usuario.php
+++ editar_usuario.php
@@ -11,7 +11,7 @@
     <div class="starter-template">
         <div class="row">
             <div class="col-md-12">
-                <form method="POST" action="processa/processa_cad_usuario.php">
+                <form method="POST" action="processa/processa_edt_usuario.php">
                     <div class="form-group row">
                         <label for="inputEmail3" class="col-sm-2 col-form-label">CPF:</label>
                         <div class="col-sm-10">
@@ -52,14 +52,28 @@
                         <label for="inputPassword3" class="col-sm-2 col-form-label">Tipo de Usuário:</label>
                         <div class="col-sm-10">
                             <select class="form-control" name="tipo">
-                                <option value="1">Administrador</option>
-                                <option value="0">Usuário Comum</option>
+                                <option>Selecione</option>
+                                <option value="1"
+                                <?php
+                                    if($resultado['tipo_usuario'] == 1){
+                                        echo 'selected';
+                                    }
+                                ?>
+                                >Administrador</option>
+                                <option value="0"
+                                <?php
+                                    if($resultado['tipo_usuario'] == 0){
+                                        echo 'selected';
+                                    }
+                                ?>
+                                >Usuário Comum</option>
                             </select>
                         </div>
                     </div>
+                    <input type="hidden" name="id" value="<?php echo $resultado['matricula']; ?>">
                     <div class="form-group row">
                         <div class="col-sm-10">
-                            <button type="submit" class="btn btn-primary">Cadastrar</button>
+                            <button type="submit" class="btn btn-primary">Salvar</button>
                         </div>
                     </div>
                 </form>
