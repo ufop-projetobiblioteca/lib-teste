7569489a48e0db39c70ed84a3197e2891b686d6f
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-11 14:44:56 -0300

    testando coluna visualizar

--- visualizar_usuario.php
+++ visualizar_usuario.php
@@ -1,9 +1,9 @@
 <?php
-    $matricula = $_GET['id'];
+$matricula = $_GET['id'];
 
-    //consulta
-    $usuario = pg_query("SELECT * FROM usuarios WHERE matricula = '$matricula' LIMIT 1");
-    $resultado = pg_fetch_assoc($usuario);
+//consulta
+$usuario = pg_query("SELECT * FROM usuarios WHERE matricula = '$matricula' LIMIT 1");
+$resultado = pg_fetch_assoc($usuario);
 ?>
 
 <div class="container">
@@ -11,46 +11,46 @@
     <div class="starter-template">
         <div class="row">
             <div class="col-md-12">
-                <div class="col-xs-3 col-sm-1 col-md-1">
-                    CPF:
+                <div class="col">
+                    <b>CPF:</b>
                 </div>
-                <div class="col-xs-9 col-sm-11 col-md-11">
+                <div class="col">
                     <?php echo $resultado['cpf']; ?>
                 </div>
-                <div class="col-xs-3 col-sm-1 col-md-1">
-                    Nome:
+                <div class="col">
+                    <b>Nome:</b>
                 </div>
-                <div class="col-xs-9 col-sm-11 col-md-11">
+                <div class="col">
                     <?php echo $resultado['pnome']; ?>
                 </div>
-                <div class="col-xs-3 col-sm-1 col-md-1">
-                    Sobrenome:
+                <div class="col">
+                    <b>Sobrenome:</b>
                 </div>
-                <div class="col-xs-9 col-sm-11 col-md-11">
+                <div class="col">
                     <?php echo $resultado['unome']; ?>
                 </div>
-                <div class="col-xs-3 col-sm-1 col-md-1">
-                    Matrícula:
+                <div class="col">
+                    <b>Matrícula:</b>
                 </div>
-                <div class="col-xs-9 col-sm-11 col-md-11">
+                <div class="col">
                     <?php echo $resultado['matricula']; ?>
                 </div>
-                <div class="col-xs-3 col-sm-1 col-md-1">
-                    E-mail:
+                <div class="col">
+                    <b>E-mail:</b>
                 </div>
-                <div class="col-xs-9 col-sm-11 col-md-11">
+                <div class="col">
                     <?php echo $resultado['email']; ?>
                 </div>
-                <div class="col-xs-3 col-sm-1 col-md-1">
-                    Senha:
+                <div class="col">
+                    <b>Senha:</b>
                 </div>
-                <div class="col-xs-9 col-sm-11 col-md-11">
+                <div class="col">
                     <?php echo $resultado['senha']; ?>
                 </div>
-                <div class="col-xs-3 col-sm-1 col-md-1">
-                    Tipo de Usuário:
+                <div class="col">
+                    <b>Tipo de Usuário:</b>
                 </div>
-                <div class="col-xs-9 col-sm-11 col-md-11">
+                <div class="col">
                     <?php echo $resultado['tipo_usuario']; ?>
                 </div>
             </div>
