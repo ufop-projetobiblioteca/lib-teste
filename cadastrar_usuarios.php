d843c9a0d5a012ae7963fc5c4338a7745c5d2c75
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-11 03:26:21 -0300

    arrumando o forms

--- cadastrar_usuarios.php
+++ cadastrar_usuarios.php
@@ -52,19 +52,30 @@ session_start();
                 <div class="col-md-12">
                     <form>
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            <label for="exampleInputEmail1">CPF</label>
+                            <input type="email" class="form-control" id="inputCpf" aria-describedby="emailHelp">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <label for="exampleInputPassword1">Nome</label>
+                            <input type="password" class="form-control" id="inputNome">
                         </div>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Sobrenome</label>
+                            <input type="email" class="form-control" id="inputSobrenome" aria-describedby="emailHelp">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Matrícula</label>
+                            <input type="password" class="form-control" id="inputMatrícula">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">E-mail</label>
+                            <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Senha</label>
+                            <input type="password" class="form-control" id="inputSenha">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Cadastrar</button>
                     </form>
                 </div>
             </div>
