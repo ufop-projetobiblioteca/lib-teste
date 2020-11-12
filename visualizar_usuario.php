3415be212c6184817dafa6f066f59e71c02cd415
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-12 01:48:21 -0300

    hidden false

--- visualizar_usuario.php
+++ visualizar_usuario.php
@@ -13,7 +13,7 @@ $resultado = pg_fetch_assoc($usuario);
             <div class="modal-header">
                 <h5 class="modal-title">Modal title</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+                    <span aria-hidden="false">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
