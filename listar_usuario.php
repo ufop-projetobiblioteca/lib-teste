29b245b2b7bee34e15004a43724892a7b78cc8e1
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-12 04:35:19 -0300

    testando nova tabela js com bootstrap

--- listar_usuario.php
+++ listar_usuario.php
@@ -7,9 +7,31 @@ $resultado_usuario = pg_query($conexao, $result_usuario);
 
 //Verificar se encontrou resultado na tabela "usuarios"
 if(($resultado_usuario) AND (pg_num_rows($resultado_usuario) != 0)){
-	while($row_usuario = pg_fetch_assoc($resultado_usuario)){
-		echo $row_usuario['pnome'] . "<br>";
-	}
+	?>
+	<table class="table table-striped table-bordered table-hover">
+		<thead>
+			<tr>
+				<th>Matrícula</th>
+				<th>Nome</th>
+                <th>Sobrenome</th>
+                <th>E-mail</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			while($row_usuario = pg_fetch_assoc($resultado_usuario)){
+				?>
+				<tr>
+					<th><?php echo $row_usuario['matricula']; ?></th>
+					<td><?php echo $row_usuario['pnome']; ?></td>
+                    <td><?php echo $row_usuario['unome']; ?></td>
+                    <td><?php echo $row_usuario['email']; ?></td>
+				</tr>
+				<?php
+			}?>
+		</tbody>
+	</table>
+<?php
 }else{
-	echo "Nenhum usuário encontrado";
+	echo "<div class='alert alert-danger' role='alert'>Nenhum usuário encontrado!</div>";
 }
--- teste.php
+++ teste.php
@@ -1,19 +1,28 @@
 <!DOCTYPE HTML>
-<html lang="pt-br">  
-    <head>  
-        <meta charset="utf-8">
-        <title>Listar com JavaScript</title>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    </head>
-    <body>
-		<span id="conteudo"></span>
-		<script>
-			$(document).ready(function () {
-				$.post('listar_usuario.php', function(retorna){
-					//Subtitui o valor no seletor id="conteudo"
-					$("#conteudo").html(retorna);
-				});
-			});
-		</script>
-    </body>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Listar com JavaScript</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Listar Usuários</h2>
+        <span id="conteudo"></span>
+    </div>
+    <script>
+        $(document).ready(function() {
+            $.post('listar_usuario.php', function(retorna) {
+                //Subtitui o valor no seletor id="conteudo"
+                $("#conteudo").html(retorna);
+            });
+        });
+    </script>
+</body>
+
 </html>
