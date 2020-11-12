d80db306c41f76e0a06bd0568bbc376701329f86
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-12 02:12:40 -0300

    copiando codigo antigo modal

--- listar_usuarios.php
+++ listar_usuarios.php
@@ -27,7 +27,7 @@ $linhas = pg_num_rows($resultado);
                         ?>
                             <td>
                                 <a class="btn btn-primary btn-sm" href="javascript: abrir();" role="button">Visualizar</a>
-                                <div class="modal" tabindex="-1">
+                                <!-- <div class="modal" tabindex="-1">
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                             <div class="modal-header">
@@ -42,6 +42,24 @@ $linhas = pg_num_rows($resultado);
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                                 <button type="button" class="btn btn-primary">Save changes</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div> -->
+                                <div id="popUp" class="modal" tabindex="-1" role="dialog">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Modal title</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+
+                                            </div>
+                                            <div class="modal-footer">
+                                                <a class="btn btn-primary" href="javascript: fechar();" role="button">Cancelar</a>
                                             </div>
                                         </div>
                                     </div>
