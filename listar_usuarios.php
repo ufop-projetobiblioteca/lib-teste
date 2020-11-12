a5c59b34f28c4c256fee273bd268076721e4a921
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-12 02:49:35 -0300

    voltando modal no listar

--- listar_usuarios.php
+++ listar_usuarios.php
@@ -26,7 +26,7 @@ $linhas = pg_num_rows($resultado);
                             echo "<td>" . $linhas['email'] . "</td>";
                         ?>
                             <td>
-                                <!--<a class="btn btn-primary btn-sm" href="javascript: abrir();" role="button">Visualizar</a>
+                                <a class="btn btn-primary btn-sm" href="javascript: abrir();" role="button">Visualizar</a>
                                  <div id="popUp" class="modal" tabindex="-1" role="dialog">
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
@@ -48,13 +48,13 @@ $linhas = pg_num_rows($resultado);
                                 </div> 
                                 <div class="modal-footer">
                                     <a class="btn btn-primary" href="javascript: fechar();" role="button">Cancelar</a>
-                                </div>-->
+                                </div>
 
                                 <a href='admin.php?link=7&id=<?php echo $linhas['matricula']; ?>'>
                                     <button type='button' class='btn btn-warning btn-sm'>Editar</button>
                                 <a href=''>
                                     <button type='button' class='btn btn-danger btn-sm'>Apagar</button>
-                            </td>";
+                            </td>
                         <?php
                             echo "</tr>";
                         }
