64ac445216f6d243d495a26604ece0c6debacf20
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-12 02:34:59 -0300

    colocando classe dentro do modal

--- listar_usuarios.php
+++ listar_usuarios.php
@@ -38,10 +38,67 @@ $linhas = pg_num_rows($resultado);
                                                 </button>
                                             </div>
                                             <div class="modal-body">
-                                                <?php 
-                                                    //$id = $linhas['matricula'];
-                                                    include("visualizar_usuario.php")
+                                                <?php
+                                                //consulta
+                                                $usuario = pg_query("SELECT * FROM usuarios WHERE matricula = '$matricula[matricula]' LIMIT 1");
+                                                $resultado = pg_fetch_assoc($usuario);
                                                 ?>
+                                                <div class="container">
+                                                    <h1>Visualizar Usuário</h1>
+                                                    <div class="starter-template">
+                                                        <div class="row">
+                                                            <div class="col-md-12">
+                                                                <div class="col-xs-3 col-sm-1 col-md-1">
+                                                                    <b>CPF:</b>
+                                                                </div>
+                                                                <div class="col-xs-9 col-sm-11 col-md-11">
+                                                                    <?php echo $resultado['cpf']; ?>
+                                                                </div>
+                                                                <div class="col-xs-3 col-sm-1 col-md-1">
+                                                                    <b>Nome:</b>
+                                                                </div>
+                                                                <div class="col-xs-9 col-sm-11 col-md-11">
+                                                                    <?php echo $resultado['pnome']; ?>
+                                                                </div>
+                                                                <div class="col-xs-3 col-sm-1 col-md-1">
+                                                                    <b>Sobrenome:</b>
+                                                                </div>
+                                                                <div class="col-xs-9 col-sm-11 col-md-11">
+                                                                    <?php echo $resultado['unome']; ?>
+                                                                </div>
+                                                                <div class="col-xs-3 col-sm-1 col-md-1">
+                                                                    <b>Matrícula:</b>
+                                                                </div>
+                                                                <div class="col-xs-9 col-sm-11 col-md-11">
+                                                                    <?php echo $resultado['matricula']; ?>
+                                                                </div>
+                                                                <div class="col-xs-3 col-sm-1 col-md-1">
+                                                                    <b>E-mail:</b>
+                                                                </div>
+                                                                <div class="col-xs-9 col-sm-11 col-md-11">
+                                                                    <?php echo $resultado['email']; ?>
+                                                                </div>
+                                                                <div class="col-xs-3 col-sm-1 col-md-1">
+                                                                    <b>Senha:</b>
+                                                                </div>
+                                                                <div class="col-xs-9 col-sm-11 col-md-11">
+                                                                    <?php echo $resultado['senha']; ?>
+                                                                </div>
+                                                                <div class="col-xs-3 col-sm-1 col-md-1">
+                                                                    <b>Tipo de Usuário:</b>
+                                                                </div>
+                                                                <div class="col-xs-9 col-sm-11 col-md-11">
+                                                                    <?php echo $resultado['tipo_usuario']; ?>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="pull-rigth">
+                                                            <a href="admin.php?link=2"><button type="button" class="btn btn-sm btn-primary">Listar</button></a>
+                                                            <a href='admin.php?link=7&id=<?php echo $resultado['matricula']; ?>'><button type="button" class="btn btn-sm btn-warning">Editar</button></a>
+                                                            <a href="#"><button type="button" class="btn btn-sm btn-danger">Apagar</button></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                             <div class="modal-footer">
                                                 <a class="btn btn-primary" href="javascript: fechar();" role="button">Cancelar</a>
@@ -51,8 +108,8 @@ $linhas = pg_num_rows($resultado);
                                 </div>
                                 <a href='admin.php?link=7&id=<?php echo $linhas['matricula']; ?>'>
                                     <button type='button' class='btn btn-warning btn-sm'>Editar</button>
-                                <a href=''>
-                                    <button type='button' class='btn btn-danger btn-sm'>Apagar</button>
+                                    <a href=''>
+                                        <button type='button' class='btn btn-danger btn-sm'>Apagar</button>
                             </td>";
                         <?php
                             echo "</tr>";
