c5c8194c7b8be2213e344df874eeaecdef06dcdf
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-13 05:10:05 -0300

    testando listar emprestimos

--- listar_emprestimos.php
+++ listar_emprestimos.php
@@ -1,46 +1,103 @@
 <?php
-$resultado = pg_query("SELECT * FROM emprestimos");
-$linhas = pg_num_rows($resultado);
+$result_emprestimos = "SELECT * FROM livros";
+$resultado_emprestimos = pg_query($conexao, $result_emprestimos);
 ?>
-<div role="main" class="container">
 
+<head>
+    <meta charset="utf-8">
+
+    <!-- Datatables CSS library -->
+    <link rel="stylesheet" type="text/css" href="css/jquery.datatables.min.css" />
+
+    <!-- Datatables CSS library -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+    <!-- Datatables CSS library -->
+    <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap4.min.css" />
+
+    <!-- Datatables Bootstrap library -->
+    <link rel="stylesheet" type="text/javascript" href="js/dataTables.bootstrap4.min.js" />
+
+    <!-- JQuery library -->
+    <script src="js/jquery-3.5.1.js"></script>
+
+    <!-- Datatables JS library -->
+    <script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#listaEmprestimos').DataTable({
+                "pagingType": "full_numbers",
+            });
+        });
+    </script>
+</head>
+
+<div role="main" class="container">
     <div class="starter-template">
         <div class="row">
             <div class="col-md-12">
-                <table id="table_id" class="table table-bordered table-hover">
+                <table id="listaEmprestimos" class="table table-striped table-bordered table-hover">
                     <thead>
                         <tr>
-                            <th scope="col">Matrícula</th>
-                            <th scope="col">Código do Exemplar</th>
-                            <th scope="col">Data de Empréstimo</th>
-                            <th scope="col">Data de Devolução</th>
-                            <th scope="col">Ações</th>
+                            <th>Matrícula</th>
+                            <th>Código do Exemplar</th>
+                            <th>Data do Empréstimo</th>
+                            <th>Data de Devolução</th>
+                            <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
-                        while ($linhas = pg_fetch_array($resultado)) {
-                            echo "<tr>";
-                            echo "<td>" . $linhas['ematricula'] . "</td>";
-                            echo "<td>" . $linhas['ecodigoexemplar'] . "</td>";
-                            echo "<td>" . $linhas['dataemprestimo'] . "</td>";
-                            echo "<td>" . $linhas['dataentrega'] . "</td>";
-                            ?>
-                            <td>
-                                <a href=''>
-                                    <button type='button' class='btn btn-success btn-sm'>Devolver</button>
-                                <a href=''>
-                                    <button type='button' class='btn btn-warning btn-sm'>Renovar</button>
-                            </td>";
+                        while ($row_emprestimos = pg_fetch_assoc($resultado_emprestimos)) {
+                        ?>
+                            <tr>
+                                <th><?php echo $row_emprestimos['ematricula']; ?></th>
+                                <td><?php echo $row_emprestimos['ecodigoexemplar']; ?></td>
+                                <td><?php echo $row_emprestimos['dataemprestimo']; ?></td>
+                                <td><?php echo $row_emprestimos['dataentrega']; ?></td>
+                                <td>
+                                    <button type="button" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#myModal<?php echo $row_emprestimos['id_livros'];?>">Visualizar</button>
+									<button type="button" class="btn btn-sm btn-outline-danger">Apagar</button>
+                                </td>
+                            </tr>
+                            <!-- Inicio Modal -->
+								<div class="modal fade" id="myModal<?php echo $row_emprestimos['id_livros']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+									<div class="modal-dialog" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+                                                <h5 class="modal-title text-center" id="myModalLabel">Dados do Empréstimo</h5>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+											</div>
+											<div class="modal-body">
+                                                <dl class="row">
+                                                    <dt class="col-sm-3">ID:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_emprestimos['ematricula']; ?></dd>
+
+                                                    <dt class="col-sm-3">Nome:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_emprestimos['ecodigoexemplar']; ?></dd>
+
+                                                    <dt class="col-sm-3">Autor:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_emprestimos['dataemprestimo']; ?></dd>
+
+                                                    <dt class="col-sm-3">Edicao:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_emprestimos['dataentrega']; ?></dd>
+                                                </dl>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <a class="btn btn-outline-success" href='#' role="button">Devolver</a>
+                                                <a class="btn btn-outline-warning" role="button" data-dismiss="modal" aria-label="Close">Renovar</a>
+                                            </div>
+										</div>
+									</div>
+								</div>
+								<!-- Fim Modal -->
                         <?php
-                            echo "</tr>";
                         }
                         ?>
                     </tbody>
                 </table>
             </div>
         </div>
-
     </div>
-
 </div>
