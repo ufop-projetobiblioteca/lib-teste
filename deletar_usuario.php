ff4f29d88fad20f06664cb304a6c4c423016b372
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-13 07:00:56 -0300

    testando nova msg de confirmação

--- deletar_usuario.php
+++ deletar_usuario.php
@@ -1,14 +1,21 @@
 <?php
-    $matricula = $_GET['id'];
+$matricula = $_GET['id'];
 
-    $result_usuario = "DELETE FROM usuarios WHERE matricula = '$matricula'";
-    $resultado_usuario = pg_query($conexao, $result_usuario);
+$result_usuario = "DELETE FROM usuarios WHERE matricula = '$matricula'";
+$resultado_usuario = pg_query($conexao, $result_usuario);
 
-    if($resultado_usuario){
-        $_SESSION['msg'] = "<p style='color:green;'> Usuário deletado com sucesso!</p>";
-        header("Location: admin.php?link=2");
-    }else{
-        $_SESSION['msg'] = "<p style='color:red;'> Erro ao deletar usuário!</p>";
-        header("Location: admin.php?link=2");
-    }
-?>
+if ($resultado_usuario) {
+    echo "
+                    <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://lib-teste.herokuapp.com/admin.php?link=2'>
+                    <script type=\"text/javascript\">
+                        alert(\"Curso alterado com Sucesso.\");
+                    </script>
+                ";
+} else {
+    echo "
+                    <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://lib-teste.herokuapp.com/admin.php?link=2'>
+                    <script type=\"text/javascript\">
+                        alert(\"Curso não foi alterado com Sucesso.\");
+                    </script>
+                ";
+}
