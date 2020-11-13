7f4c1b7edf3cb88c6b6ca10231c853807081a607
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-13 06:15:39 -0300

    excluindo modal do modal

--- listar_usuarios.php
+++ listar_usuarios.php
@@ -6,12 +6,6 @@ $resultado_usuario = pg_query($conexao, $result_usuario);
 <head>
     <meta charset="utf-8">
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-
     <!-- Datatables CSS library -->
     <link rel="stylesheet" type="text/css" href="css/jquery.datatables.min.css" />
 
@@ -37,25 +31,7 @@ $resultado_usuario = pg_query($conexao, $result_usuario);
             });
         });
     </script>
-
-    <script type="text/javascript">
-        $('#exampleModal').on('show.bs.modal', function(event) {
-            var button = $(event.relatedTarget) // Button that triggered the modal
-            var recipient = button.data('data-matricula') // Extract info from data-* attributes
-            var recipientnome = button.data('data-pnome')
-            var recipientdetalhes = button.data('data-unome')
-            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-            var modal = $(this)
-            modal.find('.modal-title').text('ID ' + recipient)
-            modal.find('#id-curso').val(recipient)
-            modal.find('#recipient-name').val(recipientnome)
-            modal.find('#detalhes').val(recipientdetalhes)
-
-        })
-    </script>
 </head>
-
 
 <div role="main" class="container">
     <div class="starter-template">
@@ -81,73 +57,41 @@ $resultado_usuario = pg_query($conexao, $result_usuario);
                                 <td><?php echo $row_usuario['unome']; ?></td>
                                 <td><?php echo $row_usuario['email']; ?></td>
                                 <td>
-                                    <button type="button" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#myModal<?php echo $row_usuario['matricula']; ?>">Visualizar</button>
-                                    <button type="button" class="btn btn-sm btn-outline-danger">Apagar</button>
+                                    <button type="button" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#myModal<?php echo $row_usuario['matricula'];?>">Visualizar</button>
+									<button type="button" class="btn btn-sm btn-outline-danger">Apagar</button>
                                 </td>
                             </tr>
                             <!-- Inicio Modal -->
-                            <div class="modal fade" id="myModal<?php echo $row_usuario['matricula']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title text-center" id="myModalLabel">Dados do Usuário</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <dl class="row">
-                                                <dt class="col-sm-3">Matrícula:</dt>
-                                                <dd class="col-sm-9"><?php echo $row_usuario['matricula']; ?></dd>
+								<div class="modal fade" id="myModal<?php echo $row_usuario['matricula']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+									<div class="modal-dialog" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+                                                <h5 class="modal-title text-center" id="myModalLabel">Dados do Usuário</h5>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+											</div>
+											<div class="modal-body">
+                                                <dl class="row">
+                                                    <dt class="col-sm-3">Matrícula:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_usuario['matricula']; ?></dd>
 
-                                                <dt class="col-sm-3">Nome:</dt>
-                                                <dd class="col-sm-9"><?php echo $row_usuario['pnome']; ?></dd>
+                                                    <dt class="col-sm-3">Nome:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_usuario['pnome']; ?></dd>
 
-                                                <dt class="col-sm-3">Sobrenome:</dt>
-                                                <dd class="col-sm-9"><?php echo $row_usuario['unome']; ?></dd>
+                                                    <dt class="col-sm-3">Sobrenome:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_usuario['unome']; ?></dd>
 
-                                                <dt class="col-sm-3">E-mail:</dt>
-                                                <dd class="col-sm-9"><?php echo $row_usuario['email']; ?></dd>
-                                            </dl>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" data-dismiss="modal" aria-label="Close" class="btn btn btn-outline-warning" data-toggle="modal" data-target="#exampleModal" data-matricula="<?php echo $row_usuario['matricula']; ?>" data-pnome="<?php echo $row_usuario['pnome']; ?>" data-unome="<?php $row_usuario['unome']; ?>">Editar</button>
-                                            <!--<a class="btn btn-outline-warning" href='admin.php?link=7&id=?php echo $row_usuario['matricula']; ?>' role="button">Editar</a> -->
-                                            <a class="btn btn-outline-danger" role="button" data-dismiss="modal" aria-label="Close">Cancelar</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Fim Modal -->
-
-                            <!--Início Modal Editar -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h4 class="modal-title" id="exampleModalLabel">Editar Usuário</h4>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <form method="POST" action="#" enctype="multipart/form-data">
-                                                <div class="form-group">
-                                                    <label for="recipient-name" class="control-label">Nome:</label>
-                                                    <input name="nome" type="text" class="form-control" id="recipient-name">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="message-text" class="control-label">Detalhes:</label>
-                                                    <textarea name="detalhes" class="form-control" id="detalhes"></textarea>
-                                                </div>
-                                                <input name="id" type="hidden" class="form-control" id="id-curso" value="">
-
-                                                <button type="button" class="btn btn-outline-success" data-dismiss="modal">Cancelar</button>
-                                                <button type="submit" class="btn btn-outline-danger">Alterar</button>
-
-                                            </form>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <!--Fim Modal Editar -->
+                                                    <dt class="col-sm-3">E-mail:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_usuario['email']; ?></dd>
+                                                </dl>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <a class="btn btn-outline-warning" href='admin.php?link=7&id=<?php echo $row_usuario['matricula']; ?>' role="button">Editar</a>
+                                                <a class="btn btn-outline-danger" role="button" data-dismiss="modal" aria-label="Close">Cancelar</a>
+                                            </div>
+										</div>
+									</div>
+								</div>
+								<!-- Fim Modal -->
                         <?php
                         }
                         ?>
