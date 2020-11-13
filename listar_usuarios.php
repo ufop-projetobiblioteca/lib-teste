f60403df299fd921e4e994deb9e1b1a80b3a89dc
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-13 03:41:15 -0300

    tirando o col-md-12

--- listar_usuarios.php
+++ listar_usuarios.php
@@ -26,13 +26,13 @@ $resultado_usuario = pg_query($conexao, $result_usuario);
     <!-- Datatables JS library -->
     <script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
 
-    
+
 
     <script>
         $(document).ready(function() {
             $('#listaUsuarios').DataTable({
-                "pagingType": "full_numbers",
-                
+                "pagingType": "full",
+
             });
         });
     </script>
@@ -41,32 +41,30 @@ $resultado_usuario = pg_query($conexao, $result_usuario);
 <div role="main" class="container">
     <div class="starter-template">
         <div class="row">
-            <div class="col-md-12">
-                <table id="listaUsuarios" class="table table-striped table-bordered table-hover">
-                    <thead>
+            <table id="listaUsuarios" class="table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Matrícula</th>
+                        <th>Nome</th>
+                        <th>Sobrenome</th>
+                        <th>E-mail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($row_usuario = pg_fetch_assoc($resultado_usuario)) {
+                    ?>
                         <tr>
-                            <th>Matrícula</th>
-                            <th>Nome</th>
-                            <th>Sobrenome</th>
-                            <th>E-mail</th>
+                            <th><?php echo $row_usuario['matricula']; ?></th>
+                            <td><?php echo $row_usuario['pnome']; ?></td>
+                            <td><?php echo $row_usuario['unome']; ?></td>
+                            <td><?php echo $row_usuario['email']; ?></td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        while ($row_usuario = pg_fetch_assoc($resultado_usuario)) {
-                        ?>
-                            <tr>
-                                <th><?php echo $row_usuario['matricula']; ?></th>
-                                <td><?php echo $row_usuario['pnome']; ?></td>
-                                <td><?php echo $row_usuario['unome']; ?></td>
-                                <td><?php echo $row_usuario['email']; ?></td>
-                            </tr>
-                        <?php
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
