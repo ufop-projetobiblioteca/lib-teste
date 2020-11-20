a0d4d439703fc9abcf7f92e0b4ee4143c8c3965e
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-20 15:09:47 -0300

    corrigindo insert e msg

--- processa/processa_cad_reserva.php
+++ processa/processa_cad_reserva.php
@@ -6,7 +6,7 @@
     $rcodigoexemplar = filter_input(INPUT_POST, 'rcodigoexemplar', FILTER_SANITIZE_STRING);
     $rdata = filter_input(INPUT_POST, 'rdata', FILTER_SANITIZE_STRING);
 
-    $result_user = "INSERT INTO emprestimos VALUES ('$rmatricula', '$rcodigoexemplar', '$rdata')";
+    $result_user = "INSERT INTO reservas VALUES ('$rmatricula', '$rcodigoexemplar', '$rdata')";
     $result_query = pg_query($conexao, $result_user);
 ?>
 <!DOCTYPE html>
@@ -20,7 +20,7 @@
     if($result_query){
         echo"
         <META HTTP-EQUIV=REFRESH CONTENT = '0;URL =
-        https://lib-teste.herokuapp.com/admin.php?link=5'>
+        https://lib-teste.herokuapp.com/admin.php?link=6'>
         <script type=\"text/javascript\">
             alert(\"Reserva realizada com Sucesso!\");
         </script>
@@ -28,7 +28,7 @@
     }else{
         echo"
         <META HTTP-EQUIV=REFRESH CONTENT = '0;URL =
-        https://lib-teste.herokuapp.com/admin.php?link=5'>
+        https://lib-teste.herokuapp.com/admin.php?link=6'>
         <script type=\"text/javascript\">
             alert(\"Erro ao realizar reserva!\");
         </script>
