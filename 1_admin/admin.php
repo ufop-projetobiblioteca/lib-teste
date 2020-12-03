1554abbae62c7f869990c3571af7dab766061a51
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-03 16:59:28 -0300

    testando icone icea

--- 1_admin/admin.php
+++ 1_admin/admin.php
@@ -94,53 +94,15 @@ include_once("../conexao.php");
     }
     ?>
 
-    <!-- Footer -->
-    <footer class="page-footer font-small special-color-dark pt-4">
-
-        <!-- Footer Elements -->
+    <footer class="footer mt-auto py-3">
         <div class="container">
-
-            <!-- Social buttons -->
-            <ul class="list-unstyled list-inline text-center">
-                <li class="list-inline-item">
-                    <a class="btn-floating btn-fb mx-1">
-                        <i class="fab fa-facebook-f"> </i>
-                    </a>
-                </li>
-                <li class="list-inline-item">
-                    <a class="btn-floating btn-tw mx-1">
-                        <i class="fab fa-twitter"> </i>
-                    </a>
-                </li>
-                <li class="list-inline-item">
-                    <a class="btn-floating btn-gplus mx-1">
-                        <i class="fab fa-google-plus-g"> </i>
-                    </a>
-                </li>
-                <li class="list-inline-item">
-                    <a class="btn-floating btn-li mx-1">
-                        <i class="fab fa-linkedin-in"> </i>
-                    </a>
-                </li>
-                <li class="list-inline-item">
-                    <a class="btn-floating btn-dribbble mx-1">
-                        <i class="fab fa-dribbble"> </i>
-                    </a>
-                </li>
-            </ul>
-            <!-- Social buttons -->
-
+            <span class="text-muted">Coloque o conteúdo do sticky footer aqui.</span>
+            <!-- Facebook -->
+            <a href="https://google.com">
+                <img src="img/icone-icea.ico"> </i>
+            </a>
         </div>
-        <!-- Footer Elements -->
-
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">© 2020 Copyright:
-            <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-        </div>
-        <!-- Copyright -->
-
     </footer>
-    <!-- Footer -->
 </body>
 
 </html>
