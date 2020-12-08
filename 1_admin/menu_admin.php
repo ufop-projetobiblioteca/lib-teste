9293a7bbc23d6b9cc375b8431a8d26357d23c51f
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-07 23:56:02 -0300

    testando navbar com footer

--- 1_admin/menu_admin.php
+++ 1_admin/menu_admin.php
@@ -1,31 +1,75 @@
 <!doctype html>
-<html lang="pt-br">
+<html lang="pt-br" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Victor Dias">
+    <link rel="icon" href="../img/icone-biblioteca.ico">
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="admin.php?link=1">Biblioteca</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <title>Template para sticky footer e navbar fixa, usando Bootstrap.</title>
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
+    <!-- Principal CSS do Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Estilos customizados para esse template -->
+    <link href="../css/sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+
+  <body class="d-flex flex-column h-100">
+
+    <header>
+      <!-- Navbar fixa -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="admin.php?link=1">Biblioteca</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+          <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gerência</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown01">
                     <a class="dropdown-item" href="admin.php?link=2">Usuários</a>
                     <a class="dropdown-item" href="admin.php?link=3">Livros</a>
                 </div>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="admin.php?link=4">Empréstimos <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+              <a class="nav-link" href="admin.php?link=4">Empréstimos</a>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="admin.php?link=5">Reservas <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+              <a class="nav-link" href="admin.php?link=5">Reservas</a>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Sair <span class="sr-only">(current)</span></a>
-            </li> 
-        </ul>
-    </div>
-</nav>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Pesquisa" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Começa o conteúdo da página -->
+    <main role="main" class="flex-shrink-0">
+      <div class="container">
+        <h1 class="mt-5">Sticky footer com navbar fixa</h1>
+        <p class="lead">Fixe um footer na parte inferior da viewport, em navegadores desktop, com estes HTML e CSS customizados. Além do mais, um navbar fixo foi adicionado com <code>padding-top: 60px;</code> e <code>body &gt; .container</code>.</p>
+        <p>Volte para o <a href="../sticky-footer">sticky footer padrão</a> sem a navbar.</p>
+      </div>
+    </main>
+
+    <footer class="footer mt-auto py-3">
+      <div class="container">
+        <span class="text-muted">Coloque o conteúdo do sticky footer aqui.</span>
+      </div>
+    </footer>
+
+    <!-- Principal JavaScript do Bootstrap
+    ================================================== -->
+    <!-- Foi colocado no final para a página carregar mais rápido -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+    <script>window.jQuery || document.write('<script src="../js/jquery-slim.min.js"><\/script>')</script>
+    <script src="../js/vendor/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
 </html>
