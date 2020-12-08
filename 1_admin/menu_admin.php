da51ddb22f725c760e7095b61dee488ccf66414a
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-08 00:00:59 -0300

    tentando corrigir footer

--- 1_admin/menu_admin.php
+++ 1_admin/menu_admin.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="pt-br" class="h-100">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -14,62 +15,65 @@
 
     <!-- Estilos customizados para esse template -->
     <link href="../css/sticky-footer-navbar.css" rel="stylesheet">
-  </head>
+</head>
 
-  <body class="d-flex flex-column h-100">
+<body class="d-flex flex-column h-100">
 
     <header>
-      <!-- Navbar fixa -->
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="admin.php?link=1">Biblioteca</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-          <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gerência</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="admin.php?link=2">Usuários</a>
-                    <a class="dropdown-item" href="admin.php?link=3">Livros</a>
-                </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="admin.php?link=4">Empréstimos</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="admin.php?link=5">Reservas</a>
-            </li>
-          </ul>
-          <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Pesquisa" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-          </form>
-        </div>
-      </nav>
+        <!-- Navbar fixa -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand" href="admin.php?link=1">Biblioteca</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gerência</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown01">
+                            <a class="dropdown-item" href="admin.php?link=2">Usuários</a>
+                            <a class="dropdown-item" href="admin.php?link=3">Livros</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin.php?link=4">Empréstimos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin.php?link=5">Reservas</a>
+                    </li>
+                </ul>
+                <form class="form-inline mt-2 mt-md-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Pesquisa" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                </form>
+            </div>
+        </nav>
     </header>
 
-    <!-- Começa o conteúdo da página -->
+    <!-- Começa o conteúdo da página 
     <main role="main" class="flex-shrink-0">
-      <div class="container">
-        <h1 class="mt-5">Sticky footer com navbar fixa</h1>
-        <p class="lead">Fixe um footer na parte inferior da viewport, em navegadores desktop, com estes HTML e CSS customizados. Além do mais, um navbar fixo foi adicionado com <code>padding-top: 60px;</code> e <code>body &gt; .container</code>.</p>
-        <p>Volte para o <a href="../sticky-footer">sticky footer padrão</a> sem a navbar.</p>
-      </div>
-    </main>
+        <div class="container">
+            <h1 class="mt-5">Sticky footer com navbar fixa</h1>
+            <p class="lead">Fixe um footer na parte inferior da viewport, em navegadores desktop, com estes HTML e CSS customizados. Além do mais, um navbar fixo foi adicionado com <code>padding-top: 60px;</code> e <code>body &gt; .container</code>.</p>
+            <p>Volte para o <a href="../sticky-footer">sticky footer padrão</a> sem a navbar.</p>
+        </div>
+    </main> -->
 
-    <footer class="footer mt-auto py-3">
-      <div class="container">
-        <span class="text-muted">Coloque o conteúdo do sticky footer aqui.</span>
-      </div>
+    <footer class="footer bg-dark mt-auto py-3">
+        <div class="container">
+            <span class="text-muted">Coloque o conteúdo do sticky footer aqui.</span>
+        </div>
     </footer>
 
     <!-- Principal JavaScript do Bootstrap
     ================================================== -->
     <!-- Foi colocado no final para a página carregar mais rápido -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
-    <script>window.jQuery || document.write('<script src="../js/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="../js/jquery-slim.min.js"><\/script>')
+    </script>
     <script src="../js/vendor/popper.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
-  </body>
+</body>
+
 </html>
