1a1a4776e7bf53393f2c2165bddfa71664855ba6
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-08 00:38:06 -0300

    testando dropdown antigo

--- 1_admin/menu_admin.php
+++ 1_admin/menu_admin.php
@@ -8,52 +8,27 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gerência</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown01">
-                        <a class="dropdown-item" href="admin.php?link=2">Usuários</a>
-                        <a class="dropdown-item" href="admin.php?link=3">Livros</a>
-                    </div>
-                </li>
-                <li class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Gerência
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="admin.php?link=2">Usuários</a>
-                        <a class="dropdown-item" href="admin.php?link=3">Livros</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin.php?link=4">Empréstimos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin.php?link=5">Reservas</a>
-                </li>
-            </ul>
-            <form class="form-inline mt-2 mt-md-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Pesquisa" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-            </form>
-        </div>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gerência</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="admin.php?link=2">Usuários</a>
+                    <a class="dropdown-item" href="admin.php?link=3">Livros</a>
+                </div>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="admin.php?link=4">Empréstimos <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="admin.php?link=5">Reservas <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Sair <span class="sr-only">(current)</span></a>
+            </li> 
+        </ul>
+    </div>
     </nav>
 </header>
-
-<!-- Começa o conteúdo da página 
-    <main role="main" class="flex-shrink-0">
-        <div class="container">
-            <h1 class="mt-5">Sticky footer com navbar fixa</h1>
-            <p class="lead">Fixe um footer na parte inferior da viewport, em navegadores desktop, com estes HTML e CSS customizados. Além do mais, um navbar fixo foi adicionado com <code>padding-top: 60px;</code> e <code>body &gt; .container</code>.</p>
-            <p>Volte para o <a href="../sticky-footer">sticky footer padrão</a> sem a navbar.</p>
-        </div>
-    </main> -->
-
-<!-- <footer class="footer bg-dark mt-auto py-3">
-    <div class="container">
-        <span class="text-muted">Coloque o conteúdo do sticky footer aqui.</span>
-    </div>
-</footer> -->
 
 </html>
