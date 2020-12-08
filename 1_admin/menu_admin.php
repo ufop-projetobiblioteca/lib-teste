19d2e6aae20dbbc974e373187c9fd00c89c26bc1
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-08 00:41:08 -0300

    tirando header menu_admin

--- 1_admin/menu_admin.php
+++ 1_admin/menu_admin.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html lang="pt-br" class="h-100">
 
-<header>
-    <!-- Navbar fixa -->
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="admin.php?link=1">Biblioteca</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+
+<!-- Navbar fixa -->
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="admin.php?link=1">Biblioteca</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gerência</a>
@@ -25,10 +25,9 @@
             </li>
             <li class="nav-item active">
                 <a class="nav-link" href="#">Sair <span class="sr-only">(current)</span></a>
-            </li> 
+            </li>
         </ul>
     </div>
-    </nav>
-</header>
+</nav>
 
 </html>
