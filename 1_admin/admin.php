7d99b7dbdb9e45c80ed979582072d3744188506f
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-08 02:17:32 -0300

    row d-flex

--- 1_admin/admin.php
+++ 1_admin/admin.php
@@ -107,7 +107,7 @@ include_once("../conexao.php");
 
 <footer class="footer bg-dark mt-auto fixed-bottom py-3">
     <div class="container text-center">
-        <div class="row align-items-center">
+        <div class="row d-flex align-items-center">
             <div class="col">
                 <a href="https://www.google.com">
                     <img border="0" src="../img/icea.png" class="img-fluid" alt="iceaLogo" width="100" height="100">
@@ -125,7 +125,7 @@ include_once("../conexao.php");
             </div>
         </div>
     </div>
-    <div class="footer-copyright bg-secundary text-center  fixed-bottom py-3">© 2020 Copyright:
+    <div class="footer-copyright bg-secundary text-center py-3">© 2020 Copyright:
         <a href="https://www.google.com"> MDBootstrap.com</a>
     </div>
 </footer>
