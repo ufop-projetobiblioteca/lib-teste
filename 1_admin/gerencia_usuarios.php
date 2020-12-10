e3c6b55bdf017e43ee59c04d9160fa82d66d0464
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-10 15:33:45 -0300

    colocando script no forms

--- 1_admin/gerencia_usuarios.php
+++ 1_admin/gerencia_usuarios.php
@@ -34,6 +34,27 @@ $row_usuario_usuario = pg_query($conexao, $result_usuario);
             });
         });
     </script>
+
+    <script>
+        // Disable form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Get the forms we want to add validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
+    </script>
 </head>
 
 <nav aria-label="breadcrumb">
